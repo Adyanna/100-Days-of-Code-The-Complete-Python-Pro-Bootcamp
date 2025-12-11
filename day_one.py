a7f491f5d2c53fe0452cc8f69d5dc73ab8056c9e
Milka Cutipa <adac-mk@hotmail.com>
2025-12-10 23:26:36 -0400

    completo proyecto one: print and variables

---
+++ day_one.py
@@ -0,0 +1,8 @@
+#proyecto day one of python
+
+print("Welcome to the band name generator.")
+city_name = input("What's the name of the city you grow up in? \n")
+#print(city_name)
+pet_name = input("What's your pet's name? \n")
+#print(pet_name)
+print("Your band name could be "+city_name+" "+pet_name)
