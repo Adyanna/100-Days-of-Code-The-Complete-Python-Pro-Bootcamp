efd6da8cd22e744040532d1b67d8ed3137a65bc8
Milka Cutipa <adac-mk@hotmail.com>
2025-12-11 12:05:40 -0400

    dia dos: realizamos una calculadora de propinas

---
+++ day_two_calculator.py
@@ -0,0 +1,10 @@
+
+
+print("Welcome to the tip calculator!")
+bill= float(input("What was the total bill? $"))
+porcent = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+tip = (porcent/100)*bill
+total =(bill+tip)/people
+print (f"Each person should pay: ${round(total,2)}")
